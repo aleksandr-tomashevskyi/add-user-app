ff8239c738ec276c8b00650da228c995c816d10b
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-09-09 11:46:08 +0300

    integrated closing sequence for popup window

--- src/components/UI/PopupOverlay.js
+++ src/components/UI/PopupOverlay.js
@@ -1,12 +1,19 @@
-import styles from './PopupOverlay.module.css'
+import styles from './PopupOverlay.module.css';
+
 
 function PopupOverlay(props){
+   function closeSequence(event){
+      console.log(event.target.getAttribute('data-item-id'))
+      if(event.target.getAttribute('data-item-id') === 'popup-window__closing-ribbon'){
+         props.popupTrigger();
+      }
+   }
    return(
-      <section className={styles['popup-window']}>
+      <section className={styles['popup-window']} onClick={closeSequence}>
       <div className={styles['popup-window__wrapper']}>
          <div className={styles['popup-window__closing-ribbon']}>
-            <svg className={styles['popup-window__closing-icon']} width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path className={styles['popup-window__closing-icon-inner']} d="M21 1L1 21M21 21L1 1L21 21Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <svg className={styles['popup-window__closing-icon']} data-item-id='popup-window__closing-ribbon' width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path className={styles['popup-window__closing-icon-inner']} d="M21 1L1 21M21 21L1 1L21 21Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
          </div>
          <div className={styles['popup-window__body']}>
