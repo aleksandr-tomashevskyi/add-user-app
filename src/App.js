6482d9b850e3db7a1265893bffe22c0162373bca
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-09-10 00:40:30 +0300

    created first validation sequence

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
-import React, {useState} from 'react';
-import './App.css';
-import FormBlock from './components/FormBlock'
-import ResultsBlock from './components/ResultsBlock'
-import PopupOverlay from './components/UI/PopupOverlay';
-import PopupWindow from './components/UI/PopupWindow';
-
+import React, { useState } from "react";
+import "./App.css";
+import FormBlock from "./components/FormBlock";
+import ResultsBlock from "./components/ResultsBlock";
+import PopupOverlay from "./components/UI/PopupOverlay";
+import PopupWindow from "./components/UI/PopupWindow";
 
 function App() {
-  const [userData, setUserData] = useState('');
+  const [userData, setUserData] = useState("");
+  const [isValid, setValidity] = useState(true);
+  const [validationResults, setValidationResults] = useState('');
 
-    function saveEntryHandler(newData){
-      setUserData((prevData)=>{
+  function saveEntryHandler(newData) {
+    if(checkEntryValidity(newData)){
+      setUserData((prevData) => {
         return [newData, ...prevData];
-      })
+      });
+    } else{
+      setValidationResults('Sorry, empty strings are not allowed');
+    }
   }
 
-  const [isValid, setValidity] = useState(true);
-
-  function checkValidity(){
+  function closePopup(){
     setValidity(!isValid);
+  }
+
+
+  function checkEntryValidity(newData) {
+    if(!newData.name || !newData.age){
+      setValidity(!isValid);
+      return false;
+    } else{
+      return true;
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <FormBlock onSaveEntry = {saveEntryHandler}></FormBlock>
-        <ResultsBlock addEntryData = {userData}></ResultsBlock>
-        <button onClick={checkValidity}>Check Validity</button>
-        {!isValid && <PopupOverlay popupTrigger={checkValidity}>
-          <PopupWindow />
-          </PopupOverlay>}
+        <FormBlock onSaveEntry={saveEntryHandler}></FormBlock>
+        <ResultsBlock addEntryData={userData}></ResultsBlock>
+        {!isValid && (
+          <PopupOverlay popupTrigger={closePopup}>
+            <PopupWindow message={validationResults}/>
+          </PopupOverlay>
+        )}
       </header>
     </div>
   );
--- src/components/UI/PopupWindow.js
+++ src/components/UI/PopupWindow.js
@@ -1,14 +1,12 @@
-import CardTitled from './CardTitled';
-import styles from './PopupWindow.module.css';
+import CardTitled from "./CardTitled";
+import styles from "./PopupWindow.module.css";
 
-function PopupWindow(){
-   return(
-      <div className={styles['popup-window']}>
-      <CardTitled cardTitle='Invalid input'>
-
-</CardTitled>
-      </div>
-   )
+function PopupWindow(props) {
+  return (
+    <div className={styles["popup-window"]}>
+      <CardTitled cardTitle="Invalid input">{props.message}</CardTitled>
+    </div>
+  );
 }
 
 export default PopupWindow;
