acb9aae7d048e07693bf3a4dbabef01dad9c3c9e
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-09-09 11:29:33 +0300

    created popup basic structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import FormBlock from './components/FormBlock'
 import ResultsBlock from './components/ResultsBlock'
+import PopupOverlay from './components/UI/PopupOverlay';
 
 function App() {
   const [userData, setUserData] = useState('');
@@ -12,11 +13,20 @@ function App() {
       })
   }
 
+  const [isValid, setValidity] = useState(true);
+
+  function checkValidity(){
+    setValidity(!isValid);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <FormBlock onSaveEntry = {saveEntryHandler}></FormBlock>
         <ResultsBlock addEntryData = {userData}></ResultsBlock>
+        <button onClick={checkValidity}>Check Validity</button>
+        {!isValid && <PopupOverlay popupTrigger={checkValidity}/>}
+        {/* <PopupOverlay showFormTrigger={isValid}></PopupOverlay> */}
       </header>
     </div>
   );
